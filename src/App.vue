7b81cc9f723e8af9569091d2d0d9512a190e5c0f
刘付领 <176784830@qq.com>
2020-09-25 17:45:47 +0800

    实现了ref，reactive

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
 <img alt="Vue logo" src="./assets/logo.png" />
 <br />
-<a-button type="primary" @click="adduser">按钮 </a-button>
+<a-button type="primary" @click="addount">按钮 </a-button>
+<br />
+{{ data1 }}
+{{ datared.data12 }}
 <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
+import {
+    reactive,
+    ref
+} from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
@@ -14,12 +21,26 @@ export default {
         HelloWorld,
     },
     setup() {
+        const data1 = ref("0");
+        const datared = reactive({
+            data12: 1,
+        });
         const adduser = () => {
+            data1++;
+            console.log(data1);
+            console.log("按钮事件");
+        };
+        const addount = () => {
+            data1.value++;
+            datared.data12 = datared.data12 * 2;
             console.log("按钮事件");
         };
 
         return {
             adduser,
+            addount,
+            data1,
+            datared,
         };
     },
 };
