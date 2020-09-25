39f1f11f97c960e02fa499d2d280a1929b5c29fa
刘付领 <176784830@qq.com>
2020-09-25 17:05:42 +0800

    test

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+
+const app = createApp(App);
+app.config.globalProperties.$axios = axios;
+
+app.use(store).use(router).mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+<dir>
+    <h2>你好</h2>
+</dir>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
