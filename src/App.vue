bd7ae4f42d04ee6dbc734a97a56c1d572a189a64
刘付领 <176784830@qq.com>
2020-09-28 09:51:25 +0800

    添加了rest 的vscode 的端口测试

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <img alt="Vue logo" src="./assets/logo.png" />
 <br />
-<a-button type="primary" @click="addount">按钮 </a-button>
+<a-button type="primary" @click="addaxios">按钮 </a-button>
 <br />
 {{ data1 }}
 {{ datared.data12 }}
@@ -10,6 +10,7 @@
 
 <script>
 import {
+    getCurrentInstance,
     reactive,
     ref
 } from "vue";
@@ -25,6 +26,9 @@ export default {
         const datared = reactive({
             data12: 1,
         });
+        const {
+            ctx
+        } = getCurrentInstance();
         const adduser = () => {
             data1++;
             console.log(data1);
@@ -35,12 +39,30 @@ export default {
             datared.data12 = datared.data12 * 2;
             console.log("按钮事件");
         };
+        const addaxios = () => {
+            addount();
+            ctx
+                .$axios({
+                    method: "post",
+                    url: "http://localhost:8088/home/post",
+                    data: {
+                        id: "requestUrl",
+                    },
+                })
+                .then((result) => {
+                    console.log(result);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        };
 
         return {
             adduser,
             addount,
             data1,
             datared,
+            addaxios,
         };
     },
 };
